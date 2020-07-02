317f3d350ec01b31706f966cbb5496f8ddc10242
andreasxp <andres.zhukov@gmail.com>
2020-07-02 12:17:28 +0300

    Add spec file for building executable files

--- nfb_studio/__main__.py
+++ nfb_studio/__main__.py
@@ -1,7 +1,7 @@
 """Startup script for nfb_studio."""
 import sys
 from PySide2.QtWidgets import QApplication
-from .main_window import MainWindow
+from nfb_studio.main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
