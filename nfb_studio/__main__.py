9f4f59c720bd3cff12ddaf82fe974e35e80d74f2
andreasxp <andres.zhukov@gmail.com>
2020-07-02 13:27:26 +0300

    Add commandline interface for opening files

--- nfb_studio/__main__.py
+++ nfb_studio/__main__.py
@@ -2,12 +2,21 @@
 import sys
 from PySide2.QtWidgets import QApplication
 from nfb_studio.main_window import MainWindow
+from nfb_studio.experiment import Experiment
 
 def main():
     app = QApplication(sys.argv)
 
     main_window = MainWindow()
     main_window.show()
+
+    # If a file was passed as a command-line argument, load it
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as file:
+            data = file.read()
+        
+        ex = Experiment.load(data)
+        main_window.setExperiment(ex)
 
     return app.exec_()
 
