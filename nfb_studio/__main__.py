1c2fa01580aa87135e9059ca8ab903727c467ff4
andreasxp <andres.zhukov@gmail.com>
2020-07-03 14:36:29 +0300

    Add a save option to file menu

--- nfb_studio/__main__.py
+++ nfb_studio/__main__.py
@@ -2,7 +2,6 @@
 import sys
 from PySide2.QtWidgets import QApplication
 from nfb_studio.main_window import MainWindow
-from nfb_studio.experiment import Experiment
 
 def main():
     app = QApplication(sys.argv)
@@ -12,11 +11,7 @@ def main():
 
     # If a file was passed as a command-line argument, load it
     if len(sys.argv) > 1:
-        with open(sys.argv[1]) as file:
-            data = file.read()
-        
-        ex = Experiment.load(data)
-        main_window.setExperiment(ex)
+        main_window.fileOpen(sys.argv[1])
 
     return app.exec_()
 
--- nfb_studio/main_window.py
+++ nfb_studio/main_window.py
@@ -1,6 +1,6 @@
 """Main window of the UI application."""
 import os
-
+from PySide2.QtGui import QKeySequence
 from PySide2.QtWidgets import QMainWindow, QFileDialog
 
 from nfb_studio.experiment import Experiment, ExperimentView
@@ -17,24 +17,35 @@ class MainWindow(QMainWindow):
         self._experiment = None
         self.setExperiment(Experiment())
 
+        self._assosiated_file = None
+
         # Menu bar -----------------------------------------------------------------------------------------------------
         menubar = self.menuBar()
         filemenu = menubar.addMenu("File")
 
-        open = filemenu.addAction("Open")
-        open.triggered.connect(self.load)
+        action_open = filemenu.addAction("Open")
+        action_open.triggered.connect(self.actionOpen)
+        action_open.setShortcut(QKeySequence.Open)
 
-        save = filemenu.addAction("Save As...")
-        save.triggered.connect(self.save)
+        action_save = filemenu.addAction("Save")
+        action_save.triggered.connect(self.actionSave)
+        action_save.setShortcut(QKeySequence.Save)
 
-        export = filemenu.addAction("Export")
-        export.triggered.connect(self.export)
+        action_save_as = filemenu.addAction("Save As...")
+        action_save_as.triggered.connect(self.actionSaveAs)
+        action_save_as.setShortcut(QKeySequence.SaveAs)
 
-        import_xml = filemenu.addAction("Import")
-        import_xml.triggered.connect(self.import_xml)
+        action_export = filemenu.addAction("Export...")
+        action_export.triggered.connect(self.actionExport)
+
+        action_import = filemenu.addAction("Import")
+        action_import.triggered.connect(self.actionImport)
 
     def experiment(self):
         return self._experiment
+
+    def associatedFile(self):
+        return self._assosiated_file
 
     def setExperiment(self, experiment):
         self._experiment = experiment
@@ -43,7 +54,7 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(experiment_view)
 
-    def export(self):
+    def actionExport(self):
         self.experiment().view().updateModel()
         data = self.experiment().export()
 
@@ -57,32 +68,31 @@ class MainWindow(QMainWindow):
         with open(file_path, "w") as file:
             file.write(data)
 
-    def save(self):
-        self.experiment().view().updateModel()
-        data = self.experiment().save()
+    def actionSave(self):
+        if self.associatedFile() is None:
+            self.actionSaveAs()
         
-        file_path = QFileDialog.getSaveFileName(filter="Experiment Files (*.nfbex)")[0]
-        if file_path == "":
+        self.fileSave(self.associatedFile())
+
+    def actionSaveAs(self):        
+        path = QFileDialog.getSaveFileName(filter="Experiment Files (*.nfbex)")[0]
+        if path == "":
             return
 
-        if os.path.splitext(file_path)[1] == "":  # No extension
-            file_path = file_path + ".nfbex"
+        if os.path.splitext(path)[1] == "":  # No extension
+            path = path + ".nfbex"
 
-        with open(file_path, "w") as file:
-            file.write(data)
+        self._assosiated_file = path
+        self.fileSave(self.associatedFile())
 
-    def load(self):
-        file_path = QFileDialog.getOpenFileName(filter="Experiment Files (*.nfbex)")[0]
-        if file_path == "":
+    def actionOpen(self):
+        path = QFileDialog.getOpenFileName(filter="Experiment Files (*.nfbex)")[0]
+        if path == "":
             return
 
-        with open(file_path) as file:
-            data = file.read()
-        
-        ex = Experiment.load(data)
-        self.setExperiment(ex)
+        self.fileOpen(path)
 
-    def import_xml(self):
+    def actionImport(self):
         file_path = QFileDialog.getOpenFileName(filter="XML Files (*.xml)")[0]
         if file_path == "":
             return
@@ -92,3 +102,18 @@ class MainWindow(QMainWindow):
         
         ex = Experiment.import_xml(data)
         self.setExperiment(ex)
+
+    def fileOpen(self, path):
+        with open(path) as file:
+            data = file.read()
+        
+        ex = Experiment.load(data)
+        self.setExperiment(ex)
+        self._assosiated_file = path
+
+    def fileSave(self, path):
+        self.experiment().view().updateModel()
+        data = self.experiment().save()
+
+        with open(path, "w") as file:
+            file.write(data)
