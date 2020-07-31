7224952915ab7ec6b12cafc7ffeb5fc75cd10203
andreasxp <andres.zhukov@gmail.com>
2020-07-31 13:57:43 +0300

    Add arrows and order to the export_wizard

--- nfb_studio/export_wizard.py
+++ nfb_studio/export_wizard.py
@@ -23,9 +23,23 @@ class SelectSequencePage(QWizardPage):
             return lambda: selectSequence(seq)
 
         for seq in scheme.graph.sequences():
-            name = ", ".join([n.title() for n in seq.nodes])
+            # Generate a label for radio button
+            # Find starting node in sequence
+            node = None
+            for node in seq.nodes:
+                if len(node.inputs[0].edges) == 0:
+                    break
+            
+            label = node.title()
+            # Go to next node in this sequence
+            while len(node.outputs[0].edges) != 0:
+                for edge in node.outputs[0].edges:
+                    if edge.targetNode() in seq.nodes:
+                        node = edge.targetNode()
+                        label = label + " → " + node.title()
+                        break
 
-            button = QRadioButton(name)
+            button = QRadioButton(label)
             button.clicked.connect(makeSelectSequence(seq))
 
             self.button_group.addButton(button)
@@ -35,11 +49,12 @@ class SelectSequencePage(QWizardPage):
 
 
 class ExportWizard(QWizard):
+    """A wizard responsible for guiding the user through the process of exporting an experiment."""
     def __init__(self, parent, experiment):
         super().__init__(parent)
         self.setWindowTitle("Experiment Export")
         self.sequence_page = SelectSequencePage(self, experiment.sequence_scheme)
-        self.addPage()
+        self.addPage(self.sequence_page)
 
     def sequence(self):
         return self.sequence_page.sequence
